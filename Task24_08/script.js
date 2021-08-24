919416ba39e56b5ae67f000f1a686279d11a0a17
aleksbuk <booker.ua@gmail.com>
2021-08-24 17:58:39 +0300

    second task for today

--- Task24_08/script.js
+++ Task24_08/script.js
@@ -8,3 +8,17 @@ function positiveSum(arr) {
 }
 let arr = [1,-4,7,12]
 console.log(positiveSum(arr)) /// 20
+
+/* Shortest Word
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+ */
+
+
+function findShort(s){
+    const result = Math.min(...s.split(' ').map((word) => word.length));
+    return result;
+}
+let s = "bitcoin take over the world maybe who knows perhaps"
+console.log(findShort(s)) //3
