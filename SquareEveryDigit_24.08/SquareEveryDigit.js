db9f6cad9ebafdc10003fe7fbc8cd5a63a69c134
VedKhorkov <vyedkh@gmail.com>
2021-08-27 12:10:46 +0300

    correction final code

--- SquareEveryDigit_24.08/SquareEveryDigit.js
+++ SquareEveryDigit_24.08/SquareEveryDigit.js
@@ -1,6 +1,7 @@
 // In this kata, you are asked to square every digit of a number and concatenate them.
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 // Note: The function accepts an integer and returns an integer
+let num = 56;
 function squareDigits(num) {
     let numString = num.toString(); /* перевод числа в строку с помощью метода toString */
     let result = []; /* создания массива для записи результатов работы с строчными значениями */
@@ -9,4 +10,4 @@ function squareDigits(num) {
     }
     return Number(result.join(''));/* с помощью join "склеиваеваем" элементы массива и с помощью Numder переводим строки опять в числа */
 }
-console.log(squareDigit(56)); /* делаем проверку в браузере */
+console.log(squareDigits(num)); /* делаем проверку в браузере */
