a20d2484d64d15b362c2c93cd7003d613fb3f225
VedKhorkov <vyedkh@gmail.com>
2021-08-27 11:43:41 +0300

    Update SquareEveryDigit.js
    great numString

--- SquareEveryDigit_24.08/SquareEveryDigit.js
+++ SquareEveryDigit_24.08/SquareEveryDigit.js
@@ -2,5 +2,6 @@
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 // Note: The function accepts an integer and returns an integer
 function squareDigits(num) {
-    return 0;
+    let numString = num.toString(); /* перевод числа в строку с помощью метода toString */
+    return;
 }
