32ff586e848a925765a86e1c2a58c39a0619409a
VedKhorkov <vyedkh@gmail.com>
2021-09-02 20:21:23 +0300

    returns the number of bits

--- BitCounting/test.js
+++ BitCounting/test.js
@@ -2,7 +2,13 @@
 // that are equal to one in the binary representation of that number.You can guarantee that input is non - negative.
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 // https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
-
+let n = 56;
 var countBits = function (n) {
-    // Program Me
+    let stringN = n.toString(2); // преобразуем число в сторку и сразу в двоичную систему
+    console.log(stringN);
+
+    return stringN;
 };
+
+console.log(countBits(56));
+
