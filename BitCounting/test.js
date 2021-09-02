d898b9705197072f1257256acb033b1d77d92d2e
VedKhorkov <vyedkh@gmail.com>
2021-09-02 20:49:03 +0300

    final code

--- BitCounting/test.js
+++ BitCounting/test.js
@@ -2,13 +2,19 @@
 // that are equal to one in the binary representation of that number.You can guarantee that input is non - negative.
 // Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 // https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
-let n = 56;
-var countBits = function (n) {
-    let stringN = n.toString(2); // преобразуем число в сторку и сразу в двоичную систему
-    console.log(stringN);
 
-    return stringN;
+var countBits = function (n) {
+    let stringN = [];
+    let sum = 0;
+    stringN = n.toString(2); // преобразуем число в сторку и сразу в двоичную систему
+    for (let i = 0; i < stringN.length; i++) { //задаем массив для подсчета элементов
+        sum += +stringN[i]; // суммируем элементы преобразуя строки в числа
+
+    }
+
+
+    return sum;
 };
 
-console.log(countBits(56));
+console.log(countBits(1234));
 
