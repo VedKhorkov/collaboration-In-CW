15afafa9603466559ca760308f4e5cdb9fbad620
VedKhorkov <vyedkh@gmail.com>
2021-09-05 19:50:45 +0300

    fix; 'true' to true

--- DetectPangram/task.js
+++ DetectPangram/task.js
@@ -5,23 +5,20 @@
 // Given a string, detect whether or not it is a pangram.Return True if it is, 
 // False if not.Ignore numbers and punctuation.
 function isPangram(string) {
-    let res = 'true'
+    let res = true;
     let stringArray = [];
     const abcArray = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']; // добавили массив с алфавитом для сравнения
     stringArray = string.toLowerCase().split([,]); // переводим всю строку в нижний регистр (toLowerCase) и преобразует строку в массив (split )
     for (let i = 0; i < abcArray.length; i++) {
         let result = stringArray.indexOf(abcArray[i]); // проверяем массив string на наличие элементов из массива ABC
         if (result === -1) {
-            let res = 'false';
-            console.log(abcArray[i]);
-            return false; // не работает просто  останавливается 
+            return false;
         }
-        //console.log('true');
+
     }
-    console.log(res);
-    // console.log(stringArray);
+
     return res
 }
 
-const a = isPangram("The quick brownp  over the lazy dog")
+const a = isPangram("The quick brown fox jumps over the lazy dog")
 console.log(a);
