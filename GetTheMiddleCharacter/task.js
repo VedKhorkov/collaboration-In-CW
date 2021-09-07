40877bc9ebc34a419927acd99ce5fc5534948850
VedKhorkov <vyedkh@gmail.com>
2021-09-07 14:03:21 +0300

    feat; add check is even

--- GetTheMiddleCharacter/task.js
+++ GetTheMiddleCharacter/task.js
@@ -3,5 +3,15 @@
 // return the middle character. If the word's length is even, return the middle 2 characters.
 
 function getMiddle(s) {
-    //Code goes here!
+    let getMiddleString = s.length; // считаем количество знаков в строке
+    if (getMiddleString % 2 == 0) { // проверяем, если делиться без остатка выполняем
+        let resultFirst = getMiddleString / 2;
+        let resultSecond = getMiddleString / 2 + 1;
+        console.log(resultFirst);
+        console.log(resultSecond);
+    }
+
+    // console.log(getMiddleString); // test
+
 }
+getMiddle('test');
